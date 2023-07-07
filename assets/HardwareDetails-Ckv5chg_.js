f560d4f3ff496ca3b15d3562a6b6bdbb0e22dad0
cyril-dai <32270423+cyril-dai@users.noreply.github.com>
2023-07-07 21:12:29 +0200

    Init

---
+++ assets/HardwareDetails-Ckv5chg_.js
@@ -0,0 +1,1 @@
+import{j as r,T as t}from"./index-DYp4fBDZ.js";const x=({description:e,schemaUrl:s,schemaDescription:a})=>r.jsxs(r.Fragment,{children:[r.jsx(t,{children:e}),s&&r.jsx("img",{src:s,className:"w-full",alt:""}),a&&r.jsx(t,{children:a})]});export{x as default};
